aef5915dfa3395b265c5ea4d2b30b3da499640f6
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2024-05-10 17:05:35 -0300

    Add files via upload

--- banco.R
+++ banco.R
@@ -1,8 +1,79 @@
-install.packages("RSQLite")
-library(RSQLite)
-library(readr)
+#Pacotes
 
-imdb <- read_csv("imdb.csv")
-con <- dbConnect(RSQLite::SQLite(), "C:/Users/UEPA_07/Documents/Mineracao-de-dados/quinto/aula/imdb.db")
+tryCatch({
+  if (!requireNamespace("RSQLite", quietly = TRUE)) {
+    install.packages("RSQLite")
+    library(RSQLite)
+  }
+  
+  if (!requireNamespace("readr", quietly = TRUE)) {
+    install.packages("readr")
+    library(readr)
+  }
+  
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    install.packages("dplyr")
+    library(dplyr)
+  }
+  
+  if (!requireNamespace("tidytext", quietly = TRUE)) {
+    install.packages("tidytext")
+    library(tidytext)
+  }
+  if (!requireNamespace("stringr", quietly = TRUE)) {
+    install.packages("stringr")
+    library(stringr)
+  }
+  
+  if (!requireNamespace("lexiconPT", quietly = TRUE)) {
+    install.packages("lexiconPT")
+    library(lexiconPT)
+  }
+  if (!requireNamespace("tm", quietly = TRUE)) {
+    install.packages("tm")
+    library(tm)
+  }
+  
+  if (!requireNamespace("rtweet", quietly = TRUE)) {
+    install.packages("rtweet")
+    library(rtweet)
+  }
+  if (!requireNamespace("wordcloud", quietly = TRUE)) {
+    install.packages("wordcloud")
+    library(wordcloud)
+  }
+  
+  if (!requireNamespace("reshape2", quietly = TRUE)) {
+    install.packages("reshape2")
+    library(reshape2)
+  }
+  print("Todos os pacotes estão instalados")
+  
+},
+error = function(e) {
+  cat("Erro:", conditionMessage(e), "\n")
+})
 
-dbWriteTable(con, "imdb_data", imdb, overwrite = TRUE)
+#Conexao com o bd
+tryCatch({
+  imdb <- read_csv("imdb.csv")
+  
+  con <-
+    dbConnect(
+      RSQLite::SQLite(),
+      "C:/Users/UEPA_07/Documents/Mineracao-de-dados/quinto/aula/imdb.db"
+    )
+  
+  dbWriteTable(con, "imdb_data", imdb, overwrite = TRUE)
+  
+  dados_tabela <- dbReadTable(con, "imdb_data")
+  
+  print(dados_tabela)
+  
+  on.exit({
+    dbDisconnect(con)
+  })
+},
+error = function(e) {
+  cat("Erro:", conditionMessage(e), "\n")
+})
